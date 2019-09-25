e37134db782888c027ac77a5cd82ffcefefd2fe4
Giovanni Curiel dos Santos <giovanni.santos@dextra-sw.com>
2019-09-24 22:09:10 -0300

    feat: improving routes

--- giovanniflix/app.py
+++ giovanniflix/app.py
@@ -1,9 +1,38 @@
 """
 app.py
 """
-from flask import Flask
+from flask import Flask, request
+from uuid import uuid4
+import json
 
 app = Flask(__name__)
+
+def print_request_info():
+    print(">> Request data: ")
+    print("Method: {}".format(request.method))
+    print("Headers: {}".format(request.headers))
+    print("Querystring: {}".format(request.query_string))
+    print("Path: {}".format(request.path))
+    print("Args: {}".format(request.args))
+
+# methods=["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"]
+# methods=["OPTIONS"]
+# methods=["TRACE", "CONNECT"]
+@app.route("/content", methods=["GET"])
+def get_all_content():
+    # Print request info
+    print_request_info()
+    return "<h1>Content listing</h1>"
+
+@app.route("/content", methods=["POST"])
+def post_content():
+    # Print request info
+    print_request_info()
+
+    content_id = uuid4()
+    print("ContentID is {}".format(content_id))
+    # Do stuff
+    return "<h1>Result</h1><br/>Content was published. ID is {}".format(content_id)
 
 @app.route("/")
 def get_root():
