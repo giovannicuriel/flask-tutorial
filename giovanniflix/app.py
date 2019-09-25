982163af4731190d98d1d64af59ffbd1f0dafa68
Giovanni Curiel dos Santos <giovanni.santos@dextra-sw.com>
2019-09-24 22:53:15 -0300

    feat: adding templates

--- giovanniflix/app.py
+++ giovanniflix/app.py
@@ -1,11 +1,14 @@
 """
 app.py
 """
-from flask import Flask, request
+from flask import Flask, request, render_template
 from uuid import uuid4
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./templates")
+
+content_storage = []
+
 
 def print_request_info():
     print(">> Request data: ")
@@ -22,21 +25,27 @@ def print_request_info():
 def get_all_content():
     # Print request info
     print_request_info()
-    return "<h1>Content listing</h1>"
+    return render_template("content.html", content_list=content_storage)
 
 @app.route("/content", methods=["POST"])
 def post_content():
     # Print request info
     print_request_info()
-
+    body = request.json
     content_id = uuid4()
-    print("ContentID is {}".format(content_id))
-    # Do stuff
+    content_title = body['title']
+    content_link = body['link']
+    content = {
+        "id": content_id,
+        "title": content_title,
+        "link": content_link
+    }
+    content_storage.append(content)
     return "<h1>Result</h1><br/>Content was published. ID is {}".format(content_id)
 
 @app.route("/")
 def get_root():
-    return "I am g-root"
+    return render_template("index.html", title="MainPage")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
